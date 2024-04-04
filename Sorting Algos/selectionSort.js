279cafc79dd37125702c48c88f57dbab74f6596c
shindeomkar123 <shindeom17@gmail.com>
2024-04-04 09:16:38 +0530

    add comment

--- Sorting Algos/selectionSort.js
+++ Sorting Algos/selectionSort.js
@@ -1,6 +1,7 @@
 const selectionSort = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
+      // Check for smaller value then swap
       if (arr[i] > arr[j]) {
         let temp = arr[i];
         arr[i] = arr[j];
