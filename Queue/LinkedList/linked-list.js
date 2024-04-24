61d3a359ad918d31ee2bd73ebf2176ad0b26ee8f
shindeomkar123 <shindeom17@gmail.com>
2024-04-24 18:42:34 +0530

    linked list added

---
+++ Queue/LinkedList/linked-list.js
@@ -0,0 +1,74 @@
+// Node -->
+//     Value
+//     pointer to next Node
+
+// You can traverse only in the one direction
+// Does not have indices
+// Having Head and Tail nodes
+// Pointer of Tail is null
+
+//Create a node
+
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+// Singly Likded List
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+    this.tail = null;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+
+  pop() {
+    if (!this.length) return;
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      //here newTail refers to previous node then we update current node
+      newTail = current;
+      current = current.next;
+    }
+    newTail.next = null;
+    this.tail = newTail;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+      return this.head;
+    }
+    return current;
+  }
+}
+
+const list = new SinglyLinkedList();
+
+list.push(1);
+list.push(13);
+list.pop();
+list.pop();
+
+console.log(list);
+list.push(11);
+list.push(23);
+console.log("=========================****===================");
+console.log(list);
